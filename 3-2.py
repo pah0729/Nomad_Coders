9a51d3fbeee34911930f4cd503f5d8c3227eac7f
gyehyun <pah0729@gmail.com>
2022-09-07 14:12:07 +0900

    python web scraper

--- 3-2.py
+++ 3-2.py
@@ -1,12 +1,32 @@
+from requests import get
+
 websites = (
     "google.com",
-    "aitbnb.com",
+    "airbnb.com",
     "https://twitter.com",
     "facebook.com",
-    "https://tikktok.com"
+    "https://tiktok.com",
+    "https://httpstat.us/502",
+    "https://httpstat.us/404",
+    "https://httpstat.us/300",
+    "https://httpstat.us/101"
 )
+
+results = {}
 
 for website in websites:
     if not website.startswith("https://"):
         website = f"https://{website}"
-    print(website)
+    response = get(website)
+    if response.status_code >= 500:
+        results[website] = "server error"
+    elif response.status_code >= 400:
+        results[website] = "client error"
+    elif response.status_code >= 300:
+        results[website] = "redirection"
+    elif response.status_code >= 200:
+        results[website] = "successful"
+    else:
+        results[website] = "information"
+
+print(results)
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+"""import turtle as t
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+t.shape('turtle')
 
+color_list = ['red', 'green', 'blue', 'black']
+for i in range(1, 100):
+    t.color(color_list[i%4])
+    t.forward(i)
+    t.right(50)
+"""
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# MEMO
